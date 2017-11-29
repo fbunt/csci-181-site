4aa497cbe07dc48057a7df6c7e16cc47157a1207
fbunt <fbunt@users.noreply.github.com>
2017-11-28 19:09:44 -0700

    Finish summer camp 2.0

--- public_html/projects/summercamp-2/scripts/register.js
+++ public_html/projects/summercamp-2/scripts/register.js
@@ -4,6 +4,10 @@ const TEEN_CUT = 17;
 // Young adult cutoff
 const YA_CUT = 25;
 
+const USERNAME_MIN_SIZE = 4;
+const USERNAME_MAX_SIZE = 12;
+
+
 /**
  * Get the element with the specified ID.
  */
@@ -32,15 +36,28 @@ function updateAgeGroup(ageInput) {
     var age = parseInt(ageInput.value);
     var text = "";
     if (Number.isInteger(age)) {
+        text = "Group: ";
         if (age >= MIN_AGE && age <= TEEN_CUT) {
-            text = "Junior";
+            text += "Juniors";
         } else if (age > TEEN_CUT && age <= YA_CUT) {
-            text = "Young Wolf"
+            text += "Young Wolves"
         } else if (age > YA_CUT) {
-            text = "Old Guard";
+            text += "Old Guard";
         }
     }
     getById("age-group").innerHTML = text;
+}
+
+function tooShort(name) {
+    return name.length < USERNAME_MIN_SIZE;
+}
+
+function tooLong(name) {
+    return name.length > USERNAME_MAX_SIZE;
+}
+
+function validateUsername(name) {
+    return !tooShort(name) && !tooLong(name);
 }
 
 /**
@@ -48,12 +65,12 @@ function updateAgeGroup(ageInput) {
  */
 function checkUsername() {
     var text = getById("form-username").value.toString();
-    if (text.length < 4) {
+    if (tooShort(text)) {
         getById("username-msg").innerHTML =
-            "Username must be at least 4 characters";
-    } else if (text.length > 12) {
+                "Username must be at least 4 characters";
+    } else if (tooLong(text)) {
         getById("username-msg").innerHTML =
-            "Username can't be more than 12 characters";
+                "Username can't be more than 12 characters";
     } else {
         getById("username-msg").innerHTML = "";
     }
@@ -63,7 +80,7 @@ function checkUsername() {
  * Displays the t shirt price based on the size selected and the length of the
  * users first name.
  *
- * @param radio the size redio button selected
+ * @param radio the t size radio button selected
  */
 function updateTShirtCost(radio) {
     // base shirt cost
@@ -73,8 +90,23 @@ function updateTShirtCost(radio) {
     // base + (inc * t-size) + length of first name
     var text = "<span class='fw-bold'>Cost</span>: $";
     text += base + (inc * parseInt(radio.value))
-        + getById("form-first-name").value.trim().length;
+            + getById("form-first-name").value.trim().length;
     getById("t-shirt-cost").innerHTML = text;
+}
+
+/**
+ * Map the value to the correct element ID
+ */
+function mapClassToContainerId(value) {
+    // Currently no need to do anything.
+    return value;
+}
+
+/**
+ * Map the value to the correct image.
+ */
+function getImageName(value) {
+    return "images/ow-heroes-" + value + ".jpg";
 }
 
 /**
@@ -83,22 +115,74 @@ function updateTShirtCost(radio) {
  *
  * @param check the checkbox most recently clicked
  */
-function displayClass(check) {
-    var container = getById(check.value);
+function displayHeroClass(check) {
+
+    var container = getById(mapClassToContainerId(check.value));
     while (container.hasChildNodes()) {
         container.removeChild(container.lastChild);
     }
     if (check.checked) {
         var img = document.createElement("img");
-        img.src = "images/ow-heroes-" + check.value + ".jpg";
-       container.appendChild(img);
+        img.src = getImageName(check.value);
+        container.appendChild(img);
+        container.style.display = "block"
+    } else {
+        // Completely hide the element
+        container.style.display = "none";
     }
 }
 
+// TODO: Set event functions here instead of in html
 function onLoad() {
     populateAgeValues();
 }
 
+
+// F O R M   V A L I D A T I O N   F U N C T I O N S
+
+function validatePersonalInfo() {
+    var fname = getById(["form-first-name"]).value.trim();
+    var lname = getById("form-last-name").value.trim();
+    var gender = getById("form-female").checked
+            || getById("form-male").checked;
+    var age = parseInt(getById("form-age").value);
+    return Boolean(fname && lname && gender && Number.isInteger(age));
+}
+
+function validateContactInfo() {
+    var email = getById("form-email").value.trim();
+    var phone = getById("form-telephone").value.trim();
+    return Boolean(email && phone);
+}
+
+function validateGeneralInfo() {
+    var uname = getById("form-username").value.trim();
+    var sizeChecked = false;
+    var radios = document.getElementsByName("t-size");
+    for (var i = 0; i < radios.length; ++i) {
+        sizeChecked |= radios[i].checked;
+    }
+    return Boolean(validateUsername(uname) && sizeChecked);
+}
+
+function validateOverwatchInfo() {
+    var checks = document.getElementsByName("form-pref-class");
+    var checked = false;
+    for (var i = 0; i < checks.length; ++i) {
+        checked |= checks[i].checked;
+    }
+    var expl = getById("form-explanation").value.trim();
+    return Boolean(checked && expl);
+}
+
 function validateForm() {
-    return true;
+    if (validatePersonalInfo()
+            && validateContactInfo()
+            && validateGeneralInfo()
+            && validateOverwatchInfo()) {
+        return true;
+    } else {
+        alert("Please complete all required fields.");
+        return false;
+    }
 }
